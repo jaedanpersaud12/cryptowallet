0dbf6ff42f0f9080975f89a149dae86731a6c555
jaedanpersaud12 <jaedanpersaud12@gmail.com>
2021-12-07 14:23:33 -0400

    adds function to stop duplication of words

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const wordArray = ['wood', 'combination', 'wealth', 'preparation', 'tooth', 'employer', 'garbage', 'tea', 'topic', 'government', 'attention', 'climate', 'election', 'performance', 'apple', 'king', 'client', 'road', 'judgment', 'road', 'ear', 'inflation', 'improvement', 'concept', 'recipe']
-var words = document.getElementsByClassName('walletpass');
+
+console.log(wordArray.length)
+
+/*var words = document.getElementsByClassName('walletpass');
 var random;
-for(var i = 0; i < words.length; i++){
-    random = Math.floor(Math.random() * 25)
-    words[i].textContent = wordArray[random];
-}
+var numberArray = [];
+for (var i = 0; i < words.length; i++) {
+  random = Math.floor(Math.random() * 25); // gets a random number from 1-24
+
+  while (numberArray.indexOf(random) >= 0) { //while the number exists in the array
+    random = Math.floor(Math.random() * 25); //tries again and the loop continues until the number doesnt exist
+  }
+  numberArray.push(random); //adds random number to array
+
+  words[i].textContent = wordArray[random];
+}*/
